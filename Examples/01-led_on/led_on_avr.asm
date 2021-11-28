77139599c095f7d58eb4f01a305531e2422ba6b6
agguro <admin@agguro.be>
2021-11-28 11:33:12 +0100

    Led on pin13 on

---
+++ Examples/01-led_on/led_on_avr.asm
@@ -0,0 +1,11 @@
+
+led_on.hex:     file format ihex
+
+
+Disassembly of section .sec1:
+
+00000000 <.sec1>:
+   0:	00 e2       	ldi	r16, 0x20	; 32
+   2:	04 b9       	out	0x04, r16	; 4
+   4:	05 b9       	out	0x05, r16	; 5
+   6:	ff cf       	rjmp	.-2      	;  0x6
